177d752022e1ad11e659a77d7faa964eabf41e86
Seng <engelhardt_stefan@gmx.de>
2022-12-01 17:17:55 +0100

    day 1

--- day1/main.go
+++ day1/main.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	"aocutil"
-	"fmt"
+	"sort"
 )
 
 func main() {
-	for s := range aocutil.FileReadAllLines("day1/main.go") {
-		fmt.Printf("%v\n", s)
-	}
+	calories := make([]int, 0, 32)
+	current := 0
+	aocutil.FileReadAllLines("input.txt", func(s string) {
+		if len(s) > 0 {
+			current += aocutil.Atoi(s)
+		} else {
+			calories = append(calories, current)
+			current = 0
+		}
+	})
 
-	aocutil.AOCFinish(10, 20)
+	sort.Slice(calories, func(i, j int) bool {
+		return calories[i] > calories[j]
+	})
+
+	part1 := calories[0]
+	part2 := calories[0] + calories[1] + calories[2]
+
+	aocutil.AOCFinish(part1, part2)
 }
