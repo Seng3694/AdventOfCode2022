6caa02ebd1593a92babee23cdda133f96dbc637e
unknown <engelhardt_stefan@gmx.de>
2022-12-02 21:00:30 +0100

    improve day 2

--- day02/main.go
+++ day02/main.go
@@ -49,43 +49,64 @@ func calculate_score(opponent, response byte) int {
 	return score
 }
 
+//rock <- paper <- scissors
+
+func wrap(value, min, max int) int {
+	if value < min {
+		return max
+	} else if value > max {
+		return min
+	} else {
+		return value
+	}
+}
+
 func main() {
 	scorePart1 := 0
 	scorePart2 := 0
 	aocutil.FileReadAllLines("input.txt", func(s string) {
 		moves := strings.Split(s, " ")
-		scorePart1 += calculate_score(moves[0][0], moves[1][0])
+		first := moves[0][0]
+		second := moves[1][0]
 
-		switch moves[0][0] {
-		case 'A':
-			switch moves[1][0] {
-			case 'X':
-				scorePart2 += 3
-			case 'Y':
-				scorePart2 += 1 + 3
-			case 'Z':
-				scorePart2 += 2 + 6
-			}
-		case 'B':
-			switch moves[1][0] {
-			case 'X':
-				scorePart2 += 1
-			case 'Y':
-				scorePart2 += 2 + 3
-			case 'Z':
-				scorePart2 += 3 + 6
-			}
-		case 'C':
-			switch moves[1][0] {
-			case 'X':
-				scorePart2 += 2
-			case 'Y':
-				scorePart2 += 3 + 3
-			case 'Z':
-				scorePart2 += 1 + 6
-			}
+		opponent := int(first - 'A' + 1)
+		player := int(second - 'X' + 1)
+
+		//both between 1 and 3 with 1 being rock, 2 being paper, 3 being scissors
+		//the higher one beats the lower one (dist 1)
+		//if dist is 2 then the lower one beast the higher one (wraps around)
+
+		//rock vs paper        1 - 2 = -1 win
+		//paper vs scissors    2 - 3 = -1 win
+		//scissors vs rock     3 - 1 = 2 win
+
+		//rock vs scissors     1 - 3 = -2 loss
+		//paper vs rock        2 - 1 = 1 loss
+		//scissors vs paper    3 - 2 = 1 loss
+
+		dist := opponent - player
+		if dist == -2 || dist == 1 { //loss
+			scorePart1 += player
+		} else if dist == 2 || dist == -1 { //win
+			scorePart1 += player + 6
+		} else { //draw
+			scorePart1 += player + 3
+		}
+
+		switch player {
+		case 1: //X lose
+			player = wrap(opponent-1, 1, 3)
+			scorePart2 += player
+		case 2: //Y draw
+			player = opponent
+			scorePart2 += player + 3
+		case 3: //Z win
+			player = wrap(opponent+1, 1, 3)
+			scorePart2 += player + 6
 		}
 	})
 
+	//14163
+	//12091
 	aocutil.AOCFinish(scorePart1, scorePart2)
 }
