3efe6a0260c0df6ec4d4db52c01fd070db3900e9
Seng <engelhardt_stefan@gmx.de>
2022-12-03 08:53:33 +0100

    simplify day 2

--- day02/main.go
+++ day02/main.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 )
 
+// assumes that the difference between min-value or value-max is smaller than max-min
 func wrap(value, min, max int) int {
 	if value < min {
 		return (max + 1) - (min - value)
@@ -15,59 +16,60 @@ func wrap(value, min, max int) int {
 	}
 }
 
+func calculate_score(opponent, player int) int {
+	score := player + 1
+	distance := wrap(opponent-player, 0, 2)
+	if distance == 2 { //win
+		score += 6
+	} else if distance == 0 { //draw
+		score += 3
+	}
+	return score
+}
+
 func main() {
-	scorePart1 := 0
-	scorePart2 := 0
+	part1 := 0
+	part2 := 0
 	aocutil.FileReadAllLines("input.txt", func(s string) {
 		moves := strings.Split(s, " ")
-		opponent := int(moves[0][0] - 'A' + 1)
-		player := int(moves[1][0] - 'X' + 1)
+		opponent := int(moves[0][0] - 'A')
+		player := int(moves[1][0] - 'X')
 
 		// PART 1
-		// both between 1 and 3 with 1 being rock, 2 being paper, 3 being scissors
-		// the higher one beats the lower one (dist 1)
-		// if dist is 2 then the lower one beats the higher one (wraps around)
+		// both between 0 and 2 with 0 being rock, 1 being paper, 2 being scissors
+		// the higher one beats the lower one (dist -1 => wraps around to 2)
+		// if dist is +1 then the lower one beats the higher one (dist -2 => wraps around to +1)
 
-		// rock vs paper        1 - 2 = -1 win
-		// paper vs scissors    2 - 3 = -1 win
-		// scissors vs rock     3 - 1 = 2 win
+		// rock vs paper        0 - 1 = -1 win
+		// paper vs scissors    1 - 2 = -1 win
+		// scissors vs rock     2 - 0 = 2 win
 
-		// rock vs scissors     1 - 3 = -2 loss
-		// paper vs rock        2 - 1 = 1 loss
-		// scissors vs paper    3 - 2 = 1 loss
+		// rock vs scissors     0 - 2 = -2 loss
+		// paper vs rock        1 - 0 = 1 loss
+		// scissors vs paper    2 - 1 = 1 loss
 
 		// draw when 0
 
-		scorePart1 += player //add player value
-		distance := wrap(opponent-player, 1, 3)
-		if distance == 2 {
-			scorePart1 += 6
-		} else if distance != 1 {
-			scorePart1 += 3
-		}
+		part1 += calculate_score(opponent, player)
 
 		// PART 2
-		// 1:rock <- 2:paper <- 3:scissors
+		// 0:rock <- 1:paper <- 2:scissors
 		// one number higher is always the one who beats the lower one
 		// losing is always in the other way around
 		// so opponent+1 is the winning move
 		// opponent-1 is the losing move
-		// wrap if it's below 1 or above 3
-
+		// wrap if it's below 0 or above 2
 		switch player {
-		case 1: //X lose
-			player = wrap(opponent-1, 1, 3)
-			scorePart2 += player
-		case 2: //Y draw
+		case 0: //X lose
+			player = wrap(opponent-1, 0, 2)
+		case 1: //Y draw
 			player = opponent
-			scorePart2 += player + 3
-		case 3: //Z win
-			player = wrap(opponent+1, 1, 3)
-			scorePart2 += player + 6
+		case 2: //Z win
+			player = wrap(opponent+1, 0, 2)
 		}
+
+		part2 += calculate_score(opponent, player)
 	})
 
-	//14163
-	//12091
-	aocutil.AOCFinish(scorePart1, scorePart2)
+	aocutil.AOCFinish(part1, part2)
 }
