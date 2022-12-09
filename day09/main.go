a36d95281c4154ef8ea3c2c442763f5912c5b7ab
Seng <engelhardt_stefan@gmx.de>
2022-12-09 08:08:32 +0100

    day 9

---
+++ day09/main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"aocutil"
+	"math"
+	"strings"
+)
+
+type vector struct {
+	x, y int
+}
+
+const (
+	LEFT = iota
+	UP
+	RIGHT
+	DOWN
+)
+
+type command struct {
+	direction, amount int
+}
+
+func direction_to_int(direction string) int {
+	switch direction {
+	case "L":
+		return LEFT
+	case "U":
+		return UP
+	case "R":
+		return RIGHT
+	case "D":
+		return DOWN
+	default:
+		return -1
+	}
+}
+
+func parse_command(line string) command {
+	split := strings.Split(line, " ")
+	return command{
+		direction: direction_to_int(split[0]),
+		amount:    aocutil.Atoi(split[1]),
+	}
+}
+
+func sign(value int) int {
+	if value > 0 {
+		return 1
+	} else if value < 0 {
+		return -1
+	} else {
+		return 0
+	}
+}
+
+func move(position *vector, direction int) {
+	switch direction {
+	case LEFT:
+		position.x--
+	case UP:
+		position.y++
+	case RIGHT:
+		position.x++
+	case DOWN:
+		position.y--
+	}
+}
+
+func main() {
+	commands := make([]command, 0, 2000)
+	aocutil.FileReadAllLines("input.txt", func(s string) {
+		commands = append(commands, parse_command(s))
+	})
+
+	rope := make([]vector, 10)
+
+	part1 := make(map[vector]bool)
+	part2 := make(map[vector]bool)
+
+	part1[rope[0]] = true
+	part2[rope[0]] = true
+
+	for _, c := range commands {
+		for s := 0; s < c.amount; s++ {
+			move(&rope[0], c.direction)
+
+			for i := 1; i < len(rope); i++ {
+				dx := rope[i-1].x - rope[i].x
+				dy := rope[i-1].y - rope[i].y
+				dist := int(math.Sqrt(float64(dx*dx + dy*dy)))
+				if dist > 1 {
+					rope[i].x += sign(dx)
+					rope[i].y += sign(dy)
+					if i == 1 {
+						part1[rope[1]] = true
+					} else if i == 9 {
+						part2[rope[9]] = true
+					}
+				}
+			}
+		}
+	}
+
+	aocutil.AOCFinish(len(part1), len(part2))
+}
